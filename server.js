6dd575d30b2703432251f009f7308d2920e66952
MichaelOrSd <michael.oreilly@keyin.com>
2022-04-19 20:00:44 -0230

    all

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
  const bodyParser = require('body-parser');
  const cors = require('cors');
  const path = require('path');
+ const expressLayouts = require('express-ejs-layouts');
 
  const app = express();
 
  app.use(bodyParser());
  app.use(cors());
+ app.use(expressLayouts);
  
  app.set('views', path.join(__dirname,'views'));
  app.set('view engine', 'ejs');
 
+ // main root page.
 app.get('/', function(req, res) {
     res.render('index', {
         people: [
@@ -18,6 +21,11 @@ app.get('/', function(req, res) {
             { name: "Jerry" }, 
         ]
     });
+});
+
+// about page. 
+app.get('/about', function(req, res) {
+    res.render('about');
 });
 
  app.listen(8000, function() {
