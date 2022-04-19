0632f1743e3b9aacba50a1252bc63ced4e4e9c25
MichaelOrSd <michael.oreilly@keyin.com>
2022-04-19 11:20:47 -0230

    ejs

---
+++ server.js
@@ -0,0 +1,20 @@
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const cors = require('cors');
+ const path = require('path');
+
+ const app = express();
+
+ app.use(bodyParser());
+ app.use(cors());
+ 
+ app.set('views', path.join(__dirname,'views'));
+ app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+ app.listen(8000, function() {
+     console.log(`Listening on port: 8000.`)
+ })
